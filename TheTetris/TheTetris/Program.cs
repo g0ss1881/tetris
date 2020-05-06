42ab882b0f28d56b5695b3b5ca59bcf789ca50a0
g0ss1881 <gossyoytube@gmail.com>
2020-05-06 22:44:35 +0300

    Created function for output a char.

--- TheTetris/TheTetris/Program.cs
+++ TheTetris/TheTetris/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace TheTetris
 {
@@ -6,7 +7,23 @@ namespace TheTetris
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+            int x1 = 2, 
+                y1 = 3;
+
+            char c1 = '■';
+
+            Drow(x1, y1, c1);
+
+            Console.ReadLine();
+        }
+
+        static void Drow(int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(c);
         }
     }
 }
