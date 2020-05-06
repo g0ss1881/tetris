6fdf0694a5dc1663941126830a076e80f45f49ad
g0ss1881 <gossyoytube@gmail.com>
2020-05-06 22:28:22 +0300

    Created empty project.

---
+++ TheTetris/TheTetris/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheTetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
